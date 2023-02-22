ecc68fff7c7f944570800d111983ca48991624df
goldsun27 <todayhonest27@gmail.com>
2023-02-22 17:10:02 +0100

    delete all files

--- src/auth.axios.config.tsx
+++
@@ -1,7 +0,0 @@
-import Axios, { AxiosInstance } from 'axios'
-
-const authInstance: AxiosInstance = Axios.create()
-
-authInstance.defaults.headers.common = { Authorization: localStorage.getItem('jwtToken') }
-
-export default authInstance
--- src/views/Address/ContractDetail/ERC20TokenTxns/index.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import ERC20 from './components/Table'
-
-export const ERC20TokenTxns: React.FC = () => {
-  const columns = ['Txn Hash', 'Age', 'From', '', 'To', 'Value', 'Token']
-  return <ERC20 columns={columns} />
-}
